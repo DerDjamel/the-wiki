6e83d3a56236b70a513d7fb9934260d9ab17781c
Dermache Mohamed Djamel <dermachedjamel@gmail.com>
2026-09-19 09:42:17 +0100

    Refactor auth forms to use shared UI components

--- src/app/auth/actions.ts
+++ src/app/auth/actions.ts
@@ -7,3 +7,43 @@ export async function logout() {
   await auth.signOut();
   redirect("/");
 }
+
+export async function signInWithEmail(
+  _prevState: { error: string } | null,
+  formData: FormData,
+) {
+  console.log({ formData });
+  const { error } = await auth.signIn.email({
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  });
+
+  if (error) {
+    return { error: error.message || "Failed to sign in. Try again" };
+  }
+
+  redirect("/");
+}
+
+export async function signUpWithEmail(
+  _prevState: { error: string } | null,
+  formData: FormData,
+) {
+  const email = formData.get("email") as string;
+
+  if (!email) {
+    return { error: "Email address must be provided." };
+  }
+
+  const { error } = await auth.signUp.email({
+    email,
+    name: formData.get("name") as string,
+    password: formData.get("password") as string,
+  });
+
+  if (error) {
+    return { error: error.message || "Failed to create account" };
+  }
+
+  redirect("/");
+}
---
+++ src/app/auth/components/signup-form.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useActionState } from "react";
+import { signUpWithEmail } from "../actions";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
+  const [state, formAction, isPending] = useActionState(signUpWithEmail, null);
+
+  return (
+    <Card {...props}>
+      <CardHeader>
+        <CardTitle>Create an account</CardTitle>
+        <CardDescription>
+          Enter your information below to create your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form action={formAction}>
+          <FieldGroup>
+            <Field>
+              <FieldLabel htmlFor="name">Full Name</FieldLabel>
+              <Input
+                id="name"
+                name="name"
+                type="text"
+                placeholder="John Doe"
+                required
+              />
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="email">Email</FieldLabel>
+              <Input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="m@example.com"
+                required
+              />
+              <FieldDescription>
+                We&apos;ll use this to contact you. We will not share your email
+                with anyone else.
+              </FieldDescription>
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="password">Password</FieldLabel>
+              <Input id="password" type="password" name="password" required />
+              <FieldDescription>
+                Must be at least 8 characters long.
+              </FieldDescription>
+            </Field>
+            <FieldGroup>
+              <Field>
+                <Button type="submit" disabled={isPending}>
+                  {isPending ? "Creating Account..." : "Create Account"}
+                </Button>
+              </Field>
+            </FieldGroup>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
